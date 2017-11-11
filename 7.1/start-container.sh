52b14e723ec702c95e9fdad051c6565165c2bdfb
Maarten de Graaf <wiejeben@gmail.com>
2017-11-11 15:39:30 +0100

    Initial commit

---
+++ 7.1/start-container.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+if [ "$XDEBUG_ENABLE" == true ] || [ "$XDEBUG_ENABLE" == 1 ]; then
+    # Enable Xdebug
+    echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+    docker-php-ext-enable xdebug
+else
+    # Disable Xdebug
+    if [ -e /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini ]; then
+        rm -f /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+    fi
+fi
+
+if [ "$REDIS_ENABLE" == true ] || [ "$REDIS_ENABLE" == 1 ]; then
+    # Enable Redis
+    docker-php-ext-enable redis
+else
+    # Disable Redis
+    if [ -e /usr/local/etc/php/conf.d/docker-php-ext-redis.ini ]; then
+        rm -f /usr/local/etc/php/conf.d/docker-php-ext-redis.ini
+    fi
+fi
+
+# Start FPM
+php-fpm
